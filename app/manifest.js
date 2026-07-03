ceb958beb7729b14ffb9815e4812b92657fc26ea
MoneyShot <53429471+DcoyA@users.noreply.github.com>
2026-07-03 16:18:57 +0900

    Create manifest.js

---
+++ app/manifest.js
@@ -0,0 +1,11 @@
+export default function manifest() {
+  return {
+    name: "회사가기 싫을지도",
+    short_name: "퇴근방",
+    description: "직장인 공감 커뮤니티",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#f43f5e",
+  };
+}
